52b9f937a5fd1e10f0dea940b29e77afb32c2081
Yevheniia Sosnovska <155542252+YESosnovska@users.noreply.github.com>
2024-08-14 20:48:05 +0300

    add Reservation and Ticket models

--- planetarium/models.py
+++ planetarium/models.py
@@ -1,4 +1,7 @@
+from django.core.exceptions import ValidationError
 from django.db import models
+
+from planetarium_service import settings
 
 
 class ShowTheme(models.Model):
@@ -41,4 +44,69 @@ class ShowSession(models.Model):
         on_delete=models.DO_NOTHING
     )
     show_time = models.DateTimeField()
+
+    def __str__(self):
+        return (f"{self.astronomy_show.title}, "
+                f"{self.planetarium_dome.name}, "
+                f"{self.show_time}")
+
+
+class Reservation(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return str(self.created_at)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+
+class Ticket(models.Model):
+    row = models.IntegerField()
+    seat = models.IntegerField()
+    show_session = models.ForeignKey(
+        ShowSession,
+        related_name="tickets",
+        on_delete=models.CASCADE
+    )
+    reservation = models.ForeignKey(
+        Reservation,
+        related_name="tickets",
+        on_delete=models.CASCADE
+    )
+
+    @staticmethod
+    def validate_ticket(row, seat, planetarium_dome, error_to_raise):
+        for ticket_attr_value, ticket_attr_name, cinema_hall_attr_name in [
+            (row, "row", "rows"),
+            (seat, "seat", "seats_in_row"),
+        ]:
+            count_attrs = getattr(planetarium_dome, cinema_hall_attr_name)
+            if not (1 <= ticket_attr_value <= count_attrs):
+                raise error_to_raise(
+                    {
+                        ticket_attr_name: f"{ticket_attr_name} "
+                        f"number must be in available range: "
+                        f"(1, {cinema_hall_attr_name}): "
+                        f"(1, {count_attrs})"
+                    }
+                )
+
+    def clean(self):
+        Ticket.validate_ticket(
+            self.row,
+            self.seat,
+            self.show_session.planetarium_dome,
+            ValidationError,
+        )
+
+    def __str__(self):
+        return f"{str(self.show_session)} ({self.row}, {self.seat})"
+
+    class Meta:
+        unique_together = ("movie_session", "row", "seat")
+        ordering = ["row", "seat"]
 
