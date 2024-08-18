cee9c1b3ed995a1778c15ba26378f986a5654638
Yevheniia Sosnovska <155542252+YESosnovska@users.noreply.github.com>
2024-08-18 15:02:25 +0300

    add base views for ShowTheme, AstronomyShow, PlanetariumDome and ShowSession

--- planetarium/views.py
+++ planetarium/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from planetarium.models import (
+    ShowTheme,
+    AstronomyShow,
+    PlanetariumDome,
+    ShowSession
+)
+from planetarium.serializers import (
+    ShowThemeSerializer,
+    AstronomyShowSerializer,
+    PlanetariumDomeSerializer,
+    ShowSessionSerializer
+)
+
+
+class ShowThemeViewSet(GenericViewSet):
+    queryset = ShowTheme.objects.all()
+    serializer_class = ShowThemeSerializer
+
+
+class AstronomyShowViewSet(GenericViewSet):
+    queryset = AstronomyShow.objects.all()
+    serializer_class = AstronomyShowSerializer
+
+
+class PlanetariumDomeViewSet(GenericViewSet):
+    queryset = PlanetariumDome.objects.all()
+    serializer_class = PlanetariumDomeSerializer
+
+
+class ShowSessionViewSet(GenericViewSet):
+    queryset = ShowSession.objects.all()
+    serializer_class = ShowSessionSerializer
