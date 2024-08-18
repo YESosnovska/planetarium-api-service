cfe4e429fe374847817795be45c065923882c491
Yevheniia Sosnovska <155542252+YESosnovska@users.noreply.github.com>
2024-08-18 15:06:41 +0300

    add urlpatterns for ShowTheme, AstronomyShow, PlanetariumDome and ShowSession

--- planetarium/urls.py
+++ planetarium/urls.py
@@ -1,2 +1,20 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from planetarium.views import (
+    ShowThemeViewSet,
+    AstronomyShowViewSet,
+    PlanetariumDomeViewSet,
+    ShowSessionViewSet
+)
+
+router = routers.DefaultRouter()
+router.register("show_themes", ShowThemeViewSet)
+router.register("astronomy_shows", AstronomyShowViewSet)
+router.register("planetarium_domes", PlanetariumDomeViewSet)
+router.register("show_sessions", ShowSessionViewSet)
+
+urlpatterns = [path("", include(router.urls))]
+
 
 app_name = "planetarium"
