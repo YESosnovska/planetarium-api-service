96c8875ade2379efb25745b8da14d101c2c95233
Yevheniia Sosnovska <155542252+YESosnovska@users.noreply.github.com>
2024-08-27 16:36:10 +0300

    update user/urls.py

--- user/urls.py
+++ user/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("api/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
     path("login/", CreateTokenView.as_view(), name="token"),
     path("me/", ManageUserView.as_view(), name="manage"),
 ]
